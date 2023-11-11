72608de26cc0aba1726d677f94012d2c7c4145a8
kody.low <kodylow7@gmail.com>
2023-11-11 11:58:48 -0600

    fix: working with conner

--- src/botimint.rs
+++ src/botimint.rs
@@ -1,22 +1,27 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use cln_rpc::ClnRpc;
 use serenity::async_trait;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
-use serenity::model::prelude::{GuildId, Message, Ready};
-use serenity::prelude::{Context, EventHandler};
+use serenity::model::interactions::application_command::ApplicationCommandInteractionData;
+use serenity::model::prelude::application_command::CommandData;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
 use tokio::sync::Mutex;
 use tracing::{error, info};
 
-use crate::commands::{cln, fm, ping};
+use crate::commands::{self, cln, fm, ping};
 use crate::fedimint_local::Fedimint;
 use crate::utils;
 
+// Botimint Structure
 pub struct Botimint {
     reqwest_client: reqwest::Client,
     cln_client: Arc<Mutex<ClnRpc>>,
     fm_client: Fedimint,
     guild_id: GuildId,
+    command_map: HashMap<String, Command>,
 }
 
 impl Botimint {
@@ -31,17 +36,18 @@ impl Botimint {
             cln_client,
             fm_client,
             guild_id,
+            command_map: HashMap::new(), // Initialize with necessary commands
         }
     }
 }
 
+// Command Enum
 enum Command {
     Ping,
     FederationId,
     ClnInfo,
     ClnListPeers,
     ClnListFunds,
-    // ClnGetConnectionString,
     ClnConnect,
     ClnNewAddr,
     Unknown,
@@ -55,7 +61,6 @@ impl From<&str> for Command {
             "cln_info" => Self::ClnInfo,
             "cln_listpeers" => Self::ClnListPeers,
             "cln_listfunds" => Self::ClnListFunds,
-            // "cln_get_connection_string" => Self::ClnGetConnectionString,
             "cln_connect" => Self::ClnConnect,
             "cln_newaddr" => Self::ClnNewAddr,
             _ => Self::Unknown,
@@ -63,33 +68,14 @@ impl From<&str> for Command {
     }
 }
 
+// EventHandler implementation for Botimint
 #[async_trait]
 impl EventHandler for Botimint {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
             info!("Received command interaction: {:#?}", command.data.name);
 
-            let content = match Command::from(command.data.name.as_str()) {
-                Command::Ping => ping::run(&command.data.options),
-                Command::FederationId => fm::id::run(&command.data.options),
-                Command::ClnInfo => cln::info::run(&command.data.options, &self.cln_client).await,
-                Command::ClnListPeers => {
-                    cln::listpeers::run(&command.data.options, &self.cln_client).await
-                }
-                Command::ClnListFunds => {
-                    cln::listfunds::run(&command.data.options, &self.cln_client).await
-                }
-                // Command::ClnGetConnectionString => {
-                //     cln::get_connection_string::run(&command.data.options,
-                // &self.cln_client).await }
-                Command::ClnConnect => {
-                    cln::connect::run(&command.data.options, &self.cln_client).await
-                }
-                Command::ClnNewAddr => {
-                    cln::newaddr::run(&command.data.options, &self.cln_client).await
-                }
-                Command::Unknown => "not implemented :(".to_string(),
-            };
+            let content = self.match_command(&command.data.name, &command.data).await;
 
             if let Err(why) = command
                 .create_interaction_response(&ctx.http, |response| {
@@ -103,29 +89,15 @@ impl EventHandler for Botimint {
             }
         }
     }
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
+
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 error!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, ctx: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
         utils::create_and_log_command(&ctx.http, ping::register).await;
@@ -135,11 +107,24 @@ impl EventHandler for Botimint {
         utils::create_and_log_command(&ctx.http, cln::listfunds::register).await;
         utils::create_and_log_command(&ctx.http, cln::connect::register).await;
         utils::create_and_log_command(&ctx.http, cln::newaddr::register).await;
+    }
+}
 
-        // if let Err(_e) =
-        //     utils::create_and_log_command(&ctx.http,
-        // cln::get_connection_string::register).await {
-        //     error!("Error creating cln_get_connection_string command");
-        // }
+impl Botimint {
+    async fn match_command(&self, command_name: &str, command_data: &CommandData) -> String {
+        match Command::from(command_name) {
+            Command::Ping => ping::run(&command_data.options),
+            Command::FederationId => fm::id::run(&command_data.options),
+            Command::ClnInfo => cln::info::run(&command_data.options, &self.cln_client).await,
+            Command::ClnListPeers => {
+                cln::listpeers::run(&command_data.options, &self.cln_client).await
+            }
+            Command::ClnListFunds => {
+                cln::listfunds::run(&command_data.options, &self.cln_client).await
+            }
+            Command::ClnConnect => cln::connect::run(&command_data.options, &self.cln_client).await,
+            Command::ClnNewAddr => cln::newaddr::run(&command_data.options, &self.cln_client).await,
+            Command::Unknown => "not implemented :(".to_string(),
+        }
     }
 }
--- src/commands/cln/newaddr.rs
+++ src/commands/cln/newaddr.rs
@@ -10,6 +10,22 @@ use tokio::sync::Mutex;
 use super::format_json;
 use crate::utils::get_option_as_string;
 
+enum AddressType {
+    Bech32,
+    P2tr,
+    All
+}
+
+impl AddressType {
+    fn as_str(&self) -> &'static str {
+        match self {
+            AddressType::Bech32 => "bech32",
+            AddressType::P2tr => "p2tr",
+            AddressType::All => "all"
+        }
+    }
+}
+
 pub async fn run(options: &[CommandDataOption], cln_client: &Arc<Mutex<ClnRpc>>) -> String {
     let addr_type = options
         .get(0)
@@ -31,13 +47,36 @@ pub async fn run(options: &[CommandDataOption], cln_client: &Arc<Mutex<ClnRpc>>)
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    let mut options = HashMap::new();
+    options.insert(
+        AddressType::Bech32.as_str(),
+        CommandOptionType::String
+    );
+    options.insert(
+        AddressType::Bech32.as_str(),
+        CommandOptionType::String
+    );
+    options.insert(
+        AddressType::Bech32.as_str(),
+        CommandOptionType::String
+    );
     command
         .name("cln_newaddr")
         .description("Get a new address for on-chain deposits to this node")
-        .create_option(|opt| {
-            opt.name("addresstype")
-                .description("The type of address to generate: bech32, p2sh-segwit, or all")
-                .kind(CommandOptionType::String)
-                .required(false)
+        .create_options(|options_builder| {
+            for (name, type) in options.iter() {
+                options_builder.create_option(|opt| {
+                    opt.name(name)
+                        .description("The type of address to generate: bech32, p2sh-segwit, or all")
+                        .kind(type)
+                        .required(false)
+                });
+            }
         })
+        // .create_option(|opt| {
+        //     opt.name("addresstype")
+        //         .description("The type of address to generate: bech32, p2sh-segwit, or all")
+        //         .kind(CommandOptionType::String)
+        //         .required(false)
+        // })
 }
